50fbe5d5c5e46e484ca8768b1317f2dd3bf7f1a8
Alf Eaton <eaton.alf@gmail.com>
2016-09-04 13:17:47 +0100

    Use Guzzle

--- fetch.php
+++ fetch.php
@@ -1,47 +1,75 @@
 <?php
 
-require __DIR__ . '/headers.php';
+require __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 
 list($command, $username) = $argv;
 
 if (!$username) {
-	exit("Usage: $command username\n");
+    exit("Usage: $command username\n");
 }
 
-$context = stream_context_create(array('http' => array('user_agent' => 'hubgit/gist-backup')));
+$client = new Client(['debug' => true]);
+
+$dir = __DIR__ . '/gists/' . $username; // output directory
+
+if (!file_exists($dir)) {
+    mkdir($dir, 0700, true);
+}
 
 $url = sprintf('https://api.github.com/users/%s/gists', $username);
-$dir = __DIR__ . '/gists-' . $username; // output directory
 
 do {
-	print $url . "\n";
+    print $url . "\n";
 
-	$json = file_get_contents($url, false, $context);
-	$items = json_decode($json);
+    $response = $client->get($url, [
+        'headers' => [
+            'User-Agent' => 'hubgit/gist-backup',
+            'Accept' => 'application/json',
+        ]
+    ]);
 
-	foreach ($items as $item) {
-		$id = $item->id;
+    $items = json_decode($response->getBody());
 
-		if (!preg_match('/^\d+$/', $id)) {
-			exit($id . ' is not an integer Gist id');
-		}
+    foreach ($items as $item) {
+        $id = $item->id;
 
-		$output = $dir . '/' . $id;
+        if (!preg_match('/^[0-9a-z]+$/', $id)) {
+            exit($id . ' is not a valid Gist id');
+        }
 
-		// TODO: updates
-		if (file_exists($output)) {
-			print $output . " already exists\n";
-			continue;
-		}
+        $output = $dir . '/' . $id;
 
-		$command = sprintf('git clone %s %s', escapeshellarg($item->git_pull_url), escapeshellarg($output));
-		print "> " . $command . "\n";
-		exec($command);
+        // TODO: updates
+        if (file_exists($output)) {
+            print $output . " already exists\n";
+            continue;
+        }
 
-		file_put_contents($output . '.json', json_encode($item));
-	}
+        $command = sprintf('git clone %s %s', escapeshellarg($item->git_pull_url), escapeshellarg($output));
+        print "> " . $command . "\n";
+        exec($command);
 
-	$links = parseLinkResponseHeaders($http_response_header);
-	$url = isset($links['next']) ? $links['next'] : null;
+        file_put_contents($output . '.json', json_encode($item));
+    }
+
+
+    $url = getNextLink($response);
 } while ($url);
 
+/**
+ * @param Response $response
+ *
+ * @return string|null
+ */
+function getNextLink(Response $response) {
+    foreach ($response->getHeader('Link') as $link) {
+        if (preg_match('/<([^>]+?)>;\s*rel="next"/', $link, $matches)) {
+            return $matches[1];
+        }
+    }
+
+    return null;
+}
